fffeef2a9403e265dce68bd8f29e0e16c9f3dd3d
Mitin Arseny <mitinarseny@gmail.com>
2017-10-07 02:53:37 +0300

    js scroll, nice sidenav

--- scroll.js
+++ scroll.js
@@ -1,28 +1,22 @@
-// Add scrollspy to <body>
-// $('body').scrollspy({target: "#sidenav", offset: 50});
-
-// Add smooth scrolling on all links inside the navbar
-// $("#sidenav a").on('click', function(event) {
-//
-//   // Make sure this.hash has a value before overriding default behavior
-//   if (this.hash !== "") {
-//
-//     // Prevent default anchor click behavior
-//     event.preventDefault();
-//
-//     // Store hash
-//     var hash = this.hash;
-//
-//     // Using jQuery's animate() method to add smooth page scroll
-//     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//     $('html, body').animate({
-//       scrollTop: $(hash).offset().top
-//     }, 800, function(){
-//
-//     // Add hash (#) to URL when done scrolling (default click behavior)
-//       window.location.hash = hash;
-//     });
-//
-//   } // End if
-//
-// });
+$(document).ready(function() {
+    // Add scrollspy to <body>
+    $('body').scrollspy({
+        target: "#sidenav-wrapper"
+    });
+    $('#sidenav').on('activate.bs.scrollspy', function () {
+        console.log(1);
+        console.log(this);
+    //     //store hash
+    //     var hash = this.hash;
+     //
+    //     // animate
+    //     $('html, body').animate({
+    //         scrollTop: $(hash).offset().top
+    //     }, 300, function(){
+     //
+    //     // when done, add hash to url
+    //     // (default click behaviour)
+    //     window.location.hash = hash;
+    //  });
+    })
+});
